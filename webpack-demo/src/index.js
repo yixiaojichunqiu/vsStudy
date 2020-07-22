364c5d0ae85b8a9a78f839f5138f5ffd96e920fc
fengdongkai <fdk785126868@qq.com>
2020-07-22 15:37:09 +0800

    loader entry output

--- webpack-demo/src/index.js
+++ webpack-demo/src/index.js
@@ -1,11 +1,20 @@
 //https://www.webpackjs.com/guides/getting-started/
 import _ from 'lodash';
+import './style.css';
+import printMe from './print.js';
+
 function component() {
     var element = document.createElement('div');
-  
+    var btn = document.createElement('button');
+
     // Lodash（目前通过一个 script 脚本引入）对于执行这一行是必需的
     element.innerHTML = _.join(['Hello', 'webpack', '和我'], ' ');
-  
+    element.classList.add('hello');  
+
+    btn.innerHTML = 'Click me and check the console!';
+    btn.onclick = printMe;
+    element.appendChild(btn);
+    
     return element;
   }
   
