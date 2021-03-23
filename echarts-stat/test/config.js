3eb16dbdd16eaf9f7524c4766597b3bdbd5c01ed
fengdongkai <fdk785126868@qq.com>
2021-03-23 19:33:09 +0800

    stat

--- echarts-stat/test/config.js
+++ echarts-stat/test/config.js
@@ -4,13 +4,13 @@ require.config({
     },
     packages: [
         {
-            main: 'ecStat',
+            main: 'ecStat',//往上找到test所在目录 进入src 找到ecStat
             location: '../src',
             name: 'ecStat'
         },
         {
             main: 'echarts',
-            location: '../echarts/dist',
+            location: '../echarts/dist',//html 引用这个config 需要上面那个工具类 结合echarts 我自己粘贴进来一份测试
             name: 'echarts'
         },
         {
